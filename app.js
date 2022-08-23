d65096074edfff24217465b5ae9a46526194f700
rdy24 <raflizardeswandiy24@gmail.com>
2022-08-23 10:36:57 +0700

    :memo: integrate chapters & add filter to courses

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const coursesRouter = require("./routes/courses");
 const paymentsRouter = require("./routes/payments");
 const refreshTokensRouter = require("./routes/refreshTokens");
 const mentorsRouter = require("./routes/mentors");
-const coursesRouter = require("./routes/courses");
+const chaptersRouter = require("./routes/chapters");
 
 const verifyToken = require("./middlewares/verifyToken");
 
@@ -32,6 +32,6 @@ app.use("/courses", coursesRouter);
 app.use("/payments", paymentsRouter);
 app.use("/refresh_tokens", refreshTokensRouter);
 app.use("/mentors", mentorsRouter);
-app.use("/courses", coursesRouter);
+app.use("/chapters", chaptersRouter);
 
 module.exports = app;
